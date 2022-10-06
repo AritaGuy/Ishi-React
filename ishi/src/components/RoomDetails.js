8801fbe3ca18177c63ca3e72eec22caf6b386e50
Sam Arita <arita.sam@student.moringaschool.com>
2022-10-06 23:54:53 +0300

    Added room details function

---
+++ ishi/src/components/RoomDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css"
+ 
+function RoomDetails(){
+ return(
+    <div id="selected-room">
+      <img src="" alt="selected room"/>
+      <p id="desc"></p>
+      <button type="button" class="btn btn-dark">Book</button>
+    </div>
+  )
+ }
+
+ export default RoomDetails
